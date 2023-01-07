dc4498f61d04e2c9cd2975b84d7885178ed37142
rgsad <rsad3262@gmail.com>
2023-01-07 22:47:40 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+    let number  = n < 0 ? -n : n;
+    let num = 0;
   
+    while (number > 0) {
+      num = num * 10 + number % 10;
+      number = Math.floor(number / 10);
+    }
+    return num;
 }
